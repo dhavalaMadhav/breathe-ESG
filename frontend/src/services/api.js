8ec8a61a2252df31d6ebb9a00c88b5fcbe1300e7
madhav dhavala <madhavdhavala0@gmail.com>
2026-05-24 21:46:30 +0530

    deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a direct, lightweight Axios instance targeting local Django API proxy
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://breathe-esg-6fzp.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
